0b37fdc6c6481838eb7aea3c5bd2702a2fc27eef
Kristian <krpaeki@tlu.ee>
2019-12-20 09:29:52 +0200

    11 prax vol.2

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-import "./Itemslist.css";
+import "./ItemsList.css";
 
 
 const ItemsList = (props) => {
